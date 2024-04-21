3463e941bb196cafcf7db267f1bd43c01e2eb6ac
ugdej <120835593+ugdej@users.noreply.github.com>
2024-04-21 17:55:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -77,7 +77,8 @@ function stopRecording() {
     translateButton.click();
   }, 1000);
   startBtn.classList.remove("btn-pulsating");
-}
+  speakAutomaticallyButton.click();
+  }
 
 function startRecording() {
   recognitionService.start();
