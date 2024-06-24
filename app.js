745aa9d8e47976ac61b15f90873811f7a4c305ff
ugdej <120835593+ugdej@users.noreply.github.com>
2024-06-24 22:28:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const speechRecognitionService = window.speechRecognition || window.webkitSpeechRecognition;
-const recognitionService = new speechRecognitionService();
+const SpeechRecognitionService = window.speechRecognition || window.webkitSpeechRecognition;
+if (!SpeechRecognitionService) {
+  console.error("This browser does not support speech recognition.");
+}
+
+const recognitionService = new SpeechRecognitionService();
 const startBtn = document.querySelector(".btn-start");
 const textLog = document.querySelector(".text-log");
 const translateButton = document.querySelector(".btn-translate");
 const translationTarget = document.querySelector("#translationTarget");
 const targetLanguageSelect = document.querySelector("#targetLanguage");
 const speakAutomaticallyButton = document.querySelector("#speakAutomaticallyButton");
-const apiKey = "AIzaSyAsqA8ZCq9yH2LXG4S935j3sM8SmGHuEfA";
+const apiKey = "AIzaSyCNXjj7hiFfvKk9zXjhx3l6-icOYxpQWNk";
 const reverseButton = document.querySelector("#reverseButton");
 const switchLanguageButton = document.querySelector("#switchLanguageButton");
 const languageSelect = document.querySelector("#language");
@@ -22,10 +26,12 @@ const languages = {
   Spanish: "es-ES",
   Russian: "ru-RU",
   Hungarian: "hu-HU",
-  
-}
+};
 
 startBtn.addEventListener("click", () => {
+  if (!recognitionService) {
+    return;
+  }
   recognitionService.lang = determineLanguage();
   recognitionService.continuous = true;
   recognitionService.onresult = handleResult;
@@ -39,26 +45,7 @@ startBtn.addEventListener("click", () => {
 
 function determineLanguage() {
   const selected = languageSelect.value;
-  switch (selected) {
-    case "Romanian":
-      return languages.Romanian;
-    case "English":
-      return languages.English;
-    case "French":
-      return languages.French;
-    case "Italian":
-      return languages.Italian;
-    case "German":
-      return languages.German;
-    case "Spanish":
-      return languages.Spanish;
-    case "Russian":
-      return languages.Russian;
-    case "Hungarian":
-      return languages.Hungarian;
-    default:
-      throw new Error("Language not supported");
-  }
+  return languages[selected] || (() => { throw new Error("Language not supported"); })();
 }
 
 function handleResult(event) {
@@ -66,7 +53,7 @@ function handleResult(event) {
   for (const result of event.results) {
     results.push(`${result[0].transcript}`);
   }
-  textLog.innerHTML += results.slice(-1)[0];
+  textLog.textContent += results.slice(-1)[0];
 }
 
 function stopRecording() {
@@ -78,25 +65,28 @@ function stopRecording() {
   }, 1000);
   startBtn.classList.remove("btn-pulsating");
 }
-  
+
 function startRecording() {
+  if (isRecording) {
+    stopRecording();
+  }
   recognitionService.start();
   startBtn.innerText = "Stop";
   isRecording = true;
-  textLog.innerHTML = '';
-  translationTarget.innerHTML = '';
+  textLog.textContent = '';
+  translationTarget.textContent = '';
   startBtn.classList.add("btn-pulsating");
 }
 
 translateButton.addEventListener("click", () => {
-  const textToTranslate = textLog.innerHTML;
+  const textToTranslate = textLog.textContent;
 
   if (textToTranslate) {
     const targetLanguageCode = targetLanguageSelect.value;
     const sourceLanguageCode = recognitionService.lang;
 
     if (sourceLanguageCode === targetLanguageCode) {
-      translationTarget.innerHTML = textToTranslate;
+      translationTarget.textContent = textToTranslate;
     } else {
       translateText(textToTranslate, sourceLanguageCode, targetLanguageCode);
     }
@@ -104,16 +94,26 @@ translateButton.addEventListener("click", () => {
 });
 
 function translateText(text, sourceLang, targetLang) {
+
+
   const apiUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
   const xhr = new XMLHttpRequest();
 
   xhr.open("POST", apiUrl, true);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const response = JSON.parse(xhr.responseText);
-      const translatedText = response.data.translations[0].translatedText;
-      translationTarget.innerHTML = translatedText;
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        if (response && response.data && response.data.translations && response.data.translations[0]) {
+          const translatedText = response.data.translations[0].translatedText;
+          translationTarget.textContent = translatedText;
+        } else {
+          console.error("Invalid response format", response);
+        }
+      } else {
+        console.error("Translation API error", xhr.status, xhr.statusText);
+      }
     }
   };
 
@@ -128,14 +128,14 @@ function translateText(text, sourceLang, targetLang) {
 }
 
 textLog.addEventListener("input", () => {
-  const textToTranslate = textLog.innerText;
+  const textToTranslate = textLog.textContent;
 
   if (textToTranslate) {
     const targetLanguageCode = targetLanguageSelect.value;
     const sourceLanguageCode = recognitionService.lang;
 
     if (sourceLanguageCode === targetLanguageCode) {
-      translationTarget.innerText = textToTranslate;
+      translationTarget.textContent = textToTranslate;
     } else {
       translateText(textToTranslate, sourceLanguageCode, targetLanguageCode);
     }
@@ -143,25 +143,21 @@ textLog.addEventListener("input", () => {
 });
 
 reverseButton.addEventListener("click", () => {
-  const textLogContent = textLog.innerHTML;
-  const translationTargetContent = translationTarget.innerHTML;
+  const textLogContent = textLog.textContent;
+  const translationTargetContent = translationTarget.textContent;
 
-  textLog.innerHTML = translationTargetContent;
-  translationTarget.innerHTML = textLogContent;
+  textLog.textContent = translationTargetContent;
+  translationTarget.textContent = textLogContent;
   switchLanguageButton.click();
 });
 
 speakAutomaticallyButton.addEventListener("click", () => {
-  const translationTarget = document.querySelector("#translationTarget");
-  const volume = 1;
-
-  if (translationTarget) {
-    const textToSpeak = translationTarget.textContent;
+  const textToSpeak = translationTarget.textContent;
+  if (textToSpeak) {
     const utterance = new SpeechSynthesisUtterance(textToSpeak);
-    utterance.volume = volume;
+    utterance.volume = 1;
 
     const targetLanguageCode = targetLanguageSelect.value;
-
     utterance.lang = targetLanguageCode;
     utterance.rate = 0.7;
 
@@ -175,11 +171,9 @@ switchLanguageButton.addEventListener("click", () => {
 
   if (selectedLanguageText !== selectedTargetLanguageText) {
     selectLanguageByText(languageSelect, selectedTargetLanguageText);
-
     selectLanguageByText(targetLanguageSelect, selectedLanguageText);
   } else {
     selectLanguageByText(targetLanguageSelect, selectedLanguageText);
-
     selectLanguageByText(languageSelect, selectedTargetLanguageText);
   }
 
@@ -189,9 +183,8 @@ switchLanguageButton.addEventListener("click", () => {
 });
 
 function selectLanguageByText(selector, languageText) {
-  const option = Array.from(selector.options).find((opt) => opt.text === languageText);
+  const option = Array.from(selector.options).find(opt => opt.text === languageText);
   if (option) {
     option.selected = true;
   }
 }
-
